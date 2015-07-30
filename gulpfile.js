ef74e0a77b4c93cf177b2a699582a87ed00388d6
Rakume Hayashi <i@fake.moe>
2015-07-30 17:22:55 +0800

    Add author

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,37 @@
-var gulp = require('gulp'),
-  clean  = require('gulp-clean'),
-  stylus = require('gulp-stylus');
+var gulp   = require('gulp'),
+    sass   = require('gulp-sass'),
+    clean  = require('gulp-clean'),
+    stylus = require('gulp-stylus'),
+    rename = require('gulp-rename'),
+    minifycss    = require('gulp-minify-css'),
+    livereload   = require('gulp-lifereload'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('clean', function() { 
   return gulp.src(['dist'], {read: false})
     .pipe(clean());
 });
 
-gulp.task('styles', function() {
+gulp.task('stylus', function() {
   return gulp.src('src/lutachu.styl')
-    .pipe(stylus({
-      compress: true
-    }))
-    .pipe(gulp.dest('dist'));
+    .pipe(stylus())
+    .pipe(gulp.dest('dist'))
+    .pipe(autoprefixer(['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']))
+    .pipe(minifycss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+
+gulp.taks('sass', function() {
+  return gulp.src('sass/lutachu.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist'))
+    .pipe(autoprefixer(['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']))
+    .pipe(minifycss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
 });
 
 gulp.task('fonts', function() {
@@ -23,3 +42,8 @@ gulp.task('fonts', function() {
 gulp.task('default', ['clean'], function() { 
   gulp.start('styles', 'fonts');
 });
+
+gulp.task('watch', function() {
+  gulp.watch('src/', ['stylus']);
+  livereload.listen();
+})
