d177ac56e9a7c21d8df3f676d9962e8afe91ded8
Rakume Hayashi <i@fake.moe>
2015-08-20 21:40:20 +0800

    Use a different way to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp   = require('gulp'),
     sass   = require('gulp-sass'),
     clean  = require('gulp-clean'),
+    concat = require('gulp-concat'),
     stylus = require('gulp-stylus'),
     rename = require('gulp-rename'),
     minifycss    = require('gulp-minify-css'),
@@ -13,12 +14,13 @@ gulp.task('clean', function() {
 });
 
 gulp.task('stylus', function() {
-  return gulp.src('src/lutachu.styl')
+  return gulp.src('src/**/*.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('dist'))
     .pipe(autoprefixer(['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']))
+    .pipe(concat('lutachu.css'))
+    .pipe(gulp.dest('dist'))
     .pipe(minifycss())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(concat('lutachu.min.css'))
     .pipe(gulp.dest('dist'))
     .pipe(livereload());
 });
@@ -26,8 +28,8 @@ gulp.task('stylus', function() {
 gulp.task('scss', function() {
   return gulp.src('sass/lutachu.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist'))
     .pipe(autoprefixer(['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']))
+    .pipe(gulp.dest('dist'))
     .pipe(minifycss())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist'))
